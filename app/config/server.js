3c776316156d09dc217b446c0b290eda039965d4
Daniel Montoya <dalejandro.montoya@gmail.com>
2021-10-04 14:56:53 -0600

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6613e59deb1735a21d884031618cecb'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5c293467-729e-45a0-baca-593f1790ae91'
+};
